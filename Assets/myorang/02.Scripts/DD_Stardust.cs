252095b230e1506f4c52c2d5ed1da9cb83b67508
myorang <eoeh92@naver.com>
2018-12-01 03:58:34 +0900

    인트로 스파인 데이터 및 프리팹 + 개인 에셋

---
+++ Assets/myorang/02.Scripts/DD_Stardust.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DD_Stardust : MonoBehaviour
+{
+    // rotation
+    public float rx, ry, rz;
+    [Range(1f, 50f)]
+    public float rspd = 1f;
+
+    // move
+    public float dx, dy;
+    [Range(1f, 50f)]
+    public float movespd = 1f;
+
+
+    private void Start()
+    {
+        rx = Random.value;
+        ry = Random.value;
+        rz = Random.value;
+
+        dx = Random.value;
+        dy = Random.value;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(rx * rspd * Time.deltaTime, ry * rspd * Time.deltaTime, rz * rspd * Time.deltaTime);
+        transform.Translate(dx * movespd * Time.deltaTime, dy * movespd * Time.deltaTime, 0f);
+    }
+
+    public void WallOutReset()
+    {
+        Vector2 rdir = new Vector2(dx, dy) * -1f;
+        Ray2D ray = new Ray2D(transform.position, rdir);
+        RaycastHit2D hit = Physics2D.Raycast(transform.position, rdir, 30f, ~(1<<2));
+        transform.position = hit.point;
+        Debug.Log("Wall out reset" + hit.point + hit.collider.name);
+    }
+}
---
+++ Assets/myorang/02.Scripts/SpaceArea.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpaceArea : MonoBehaviour
+{
+    public List<DD_Stardust> stardusts = new List<DD_Stardust>();
+
+    private void OnTriggerExit(Collider other)
+    {
+        Debug.Log("who wall out");
+        if (other.name.Contains("Stardust"))
+        {
+            Debug.Log("who wall out");
+            other.SendMessage("WallOutReset");
+        }
+    }
+}
