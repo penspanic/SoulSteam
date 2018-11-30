434addc0107541a22c50de3b22ccbcd431e524be
GeunHee-Park <penspanic@gmail.com>
2018-12-01 01:22:28 +0900

    [박근희] Common 추가, Shake 추가, DoTween 세팅

---
+++ Assets/Scripts/Common/StaticData/GameRuleInfo.cs
@@ -0,0 +1,12 @@
+﻿using Common.Logic;
+using System.Collections.Generic;
+
+namespace Common.StaticData
+{
+    [System.Serializable]
+    public class GameRuleInfo
+    {
+        public float DefaultUnitCreateInterval;
+        public float PotionCreateInterval;
+    }
+}
