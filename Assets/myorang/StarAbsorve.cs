bfedebebc81e631d7e1805fd7da94c72fe29085f
myorang <eoeh92@naver.com>
2018-12-02 08:59:08 +0900

    [김대도] 커밋 - 카메라 최대 줌 줄이고, 스타앱졸브 - 항성 합체, 스타 - 이동기능 - 더스트 경험치X - 행성 경험치O

--- Assets/myorang/StarAbsorve.cs
+++ Assets/myorang/StarAbsorve.cs
@@ -11,20 +11,49 @@ public class StarAbsorve : MonoBehaviour
 		_star = transform.parent.GetComponent<Star>();
 		_star.SetAbsorve(this);
 	}
+    
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Gravity"))
+            return;
 
-	private void OnTriggerEnter2D(Collider2D other)
-	{
-		Entity otherEntity = other.GetComponent<Entity>();
+        Entity otherEntity = other.transform?.parent?.GetComponent<Entity>();
 		if (otherEntity == null)
-		{
 			return;
-		}
 
-		// 상위개체와 충돌함 (Star, Blackhole)
-		if (otherEntity.Type > _star.Type)
+        // 상위개체와 충돌함 (Star, Blackhole)
+        if (otherEntity.Type > _star.Type)
 			return;
 
 		// 동일개체와 충돌함 (Planet)
+        if(otherEntity.Type == _star.Type)
+        {
+            if(otherEntity.level == _star.level)
+            {
+                if (otherEntity.Serial > _star.Serial)
+                {
+                    EntityManager.Instance.Destroy<Star>(_star);
+                    otherEntity.level++;
+                    otherEntity.OnChangeLevel();
+                    return;
+                }
+                else
+                {
+                    EntityManager.Instance.Destroy<Star>((Star)otherEntity);
+                    _star.level++;
+                    _star.OnChangeLevel();
+                    return;
+                }
+            }
+            else if (otherEntity.level < _star.level)
+            {
+                EntityManager.Instance.Destroy<Star>((Star)otherEntity);
+                _star.level++;
+                _star.OnChangeLevel();
+                return;
+            }
+            return;
+        }
 
 		// 하위개체와 추돌함 (Dust)
 		switch (otherEntity.Type)
@@ -32,9 +61,9 @@ public class StarAbsorve : MonoBehaviour
 			case EntityType.Undefined:
 				break;
 
-			case EntityType.Dust:
-				Dust dust = (Dust)otherEntity;
-				_star.CollectDust(dust);
+			case EntityType.Planet:
+                Planet otherPlanet = (Planet)otherEntity;
+				_star.CollectPlanet(otherPlanet);
 				break;
 
 			default:
